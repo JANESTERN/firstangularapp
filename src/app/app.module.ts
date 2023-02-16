b584fc12b7644e450a1bf22d43a5d24dcbf62f03
unknown <wrightkuku10@gmail.com>
2023-02-16 19:59:38 +0300

    first angular app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { headerComponent } from 'src/header/header.component';
 
 import { AppComponent } from './app.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    headerComponent,
+    BodyComponent,
+    FooterComponent,
+  
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+  
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
